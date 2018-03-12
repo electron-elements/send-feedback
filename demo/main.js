32e03364fa832d00ead6a22ab4bb0da89480f06f
Priyank Patel <priyank3219@gmail.com>
2018-03-11 20:38:07 -0400

    Initial code

---
+++ demo/main.js
@@ -0,0 +1,17 @@
+const electron = require('electron');
+const { app, BrowserWindow } = electron;
+
+let demoWin = null;
+app.on('ready', () => {
+  demoWin = new BrowserWindow();
+  demoWin.webContents.openDevTools();
+  demoWin.loadURL(`file:///${__dirname}/index.html`);
+
+  demoWin.on('closed', () => {
+    demoWin = null;
+  });
+});
+
+app.on('quit', () => {
+  app.quit();
+});
