a79266b680753bcf9cde2927510889e987e10c0e
Priyank Patel <priyankp390@gmail.com>
2018-03-13 14:49:06 -0400

    css: allow adding custom css and remove default styling

--- demo/index.js
+++ demo/index.js
@@ -1,6 +1,6 @@
 const SendFeedback = require('../lib');
-const reportIssue = document.querySelector('send-issue-feedback');
+const sendFeedback = document.querySelector('send-issue-feedback');
 
+SendFeedback.removeDefaultStyles = true;
 customElements.define('send-issue-feedback', SendFeedback);
-reportIssue.reportIssueURL = 'https://github.com/cPhost/send-feedback/issues/new';
-window.templates = SendFeedback.templates;
+sendFeedback.reportIssueURL = 'https://github.com/cPhost/send-feedback/issues/new';
