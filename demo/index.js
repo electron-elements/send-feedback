0ffb6e98fb38e380aeb03d3ada15bd343d2e7c3e
Priyank Patel <priyankp390@gmail.com>
2018-03-13 12:33:32 -0400

    lint: turn on linter for demo directory

--- demo/index.js
+++ demo/index.js
@@ -1,6 +1,6 @@
 const SendFeedback = require('../lib');
 const reportIssue = document.querySelector('send-issue-feedback');
-const templates = SendFeedback.templates;
 
 customElements.define('send-issue-feedback', SendFeedback);
-reportIssue.reportIssueURL = 'https://github.com/cPhost/send-feedback/issues/new'
+reportIssue.reportIssueURL = 'https://github.com/cPhost/send-feedback/issues/new';
+window.templates = SendFeedback.templates;
