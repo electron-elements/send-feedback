ade8d41d4d95e564a62b30e14519d90be944febc
Priyank Patel <priyankp390@gmail.com>
2018-03-13 15:22:38 -0400

    demo: do not remove default styles in demo app

--- demo/index.js
+++ demo/index.js
@@ -1,6 +1,7 @@
 const SendFeedback = require('../lib');
 const sendFeedback = document.querySelector('send-issue-feedback');
 
-SendFeedback.removeDefaultStyles = true;
+// example of removing default styles
+// SendFeedback.removeDefaultStyles = true;
 customElements.define('send-issue-feedback', SendFeedback);
 sendFeedback.reportIssueURL = 'https://github.com/cPhost/send-feedback/issues/new';
