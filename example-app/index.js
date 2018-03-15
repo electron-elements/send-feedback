317f2bcd82c92c6951fabc1b80d5f41dd2bd62e7
Priyank Patel <priyankp390@gmail.com>
2018-03-14 20:40:09 -0400

    example-app: fix old code

--- example-app/index.js
+++ example-app/index.js
@@ -4,4 +4,4 @@ const sendFeedback = document.querySelector('send-issue-feedback');
 // example of removing default styles
 // SendFeedback.removeDefaultStyles = true;
 customElements.define('send-issue-feedback', SendFeedback);
-sendFeedback.reportIssueURL = 'https://github.com/cPhost/send-feedback/issues/new';
+console.log(sendFeedback);
