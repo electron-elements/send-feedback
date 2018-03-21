768eb946608021107e9c46cc659d1df02c63c3fc
Priyank Patel <priyankp390@gmail.com>
2018-03-21 10:59:33 -0400

    example-app: adjust the initial size of app

--- example-app/main.js
+++ example-app/main.js
@@ -3,10 +3,13 @@ const { app, BrowserWindow } = electron;
 
 let demoWin = null;
 app.on('ready', () => {
-  demoWin = new BrowserWindow();
+  demoWin = new BrowserWindow({
+    width: 1020,
+    height: 600
+  });
+
   demoWin.webContents.openDevTools();
   demoWin.loadURL(`file:///${__dirname}/send-feedback.html`);
-
   demoWin.on('closed', () => {
     demoWin = null;
   });
